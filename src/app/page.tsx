d1f471d73036df33d7c92d43805314387f683833
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2024-01-25 14:37:03 +0700

    removed bloating stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import dynamic from 'next/dynamic'
 import styles from './page.module.css'
-
-const TerminalScreen = dynamic(() => import('./terminal'), { ssr: false })
 
 const Home = () => {
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
-        <code className={styles.code}>rebuilding in progress</code>
+      <div className={styles.description}>
+        <div className={styles.center}>
+          <a href={'https://github.com/padulkemid/revamped'} target={'_blank'} rel={'noopener noreferrer'}>old blog repo</a>
+        </div>
+        <div className={styles.center}>
+          <p><br />rebuilding in go + htmx</p>
+        </div>
+        <div className={styles.center}>
+          <code className={styles.code}>@padulkemid everywhere</code>
+        </div>
       </div>
-      <TerminalScreen />
     </main>
   )
 }
